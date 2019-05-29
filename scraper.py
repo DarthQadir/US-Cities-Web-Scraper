f2ccbeb1fb1a7c022f2c50e1a293d5e4f45ee2f0
DarthQadir <darthqadir@live.com>
2019-05-29 17:33:17 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -122,4 +122,4 @@ for link in links:
                 elevation.append(remove_unicode(x.find('td').text))
                 
 #Add elevation data
-add_data('city wiki with column.csv','city wiki with elevation data.csv',elevation_data)
+add_data('city wiki with column.csv','city wiki with elevation data.csv',elevation)
